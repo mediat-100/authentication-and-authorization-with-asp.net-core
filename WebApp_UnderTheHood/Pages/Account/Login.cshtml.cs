7bba524a7525b4ab56ecdebecee69ead93a1fec6
mediat-100 <yusuftomiwa740@gmail.com>
2023-03-04 13:52:50 +0100

    Add project files.

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                // creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                    new Claim("Department", "HR"),
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true"),
+                    new Claim("EmploymentDate", "2022-02-01")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = Credential.RememberMe
+                };
+
+                // encryption of cookie
+                await HttpContext.SignInAsync("MyCookieAuth", principal, authProperties);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ WebApp_UnderTheHood/Pages/HRManagerProbationRequirement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApp_UnderTheHood.Pages
+{
+    public class HRManagerProbationRequirement : IAuthorizationRequirement
+    {
+        public HRManagerProbationRequirement(int probationMonths)
+        {
+            ProbationMonths = probationMonths;
+        }
+
+        public int ProbationMonths { get; }
+    }
+
+    public class HRManagerProbationRequirementHandler : AuthorizationHandler<HRManagerProbationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HRManagerProbationRequirement requirement)
+        {
+            if (!context.User.HasClaim(x => x.Type == "EmploymentDate"))
+                return Task.CompletedTask;
+
+            var empDate = DateTime.Parse(context.User.FindFirst(x => x.Type == "EmploymentDate").Value);
+            var period = DateTime.Now - empDate;
+
+            if (period.Days > 30 * requirement.ProbationMonths)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
